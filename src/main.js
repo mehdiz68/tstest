ae92dc156c654f8e2c4e8aae1efc206285ebbab5
mehdizanganeh <mehdi.zanganeh@gmail.com>
2025-05-06 11:00:52 +0330

    maints

--- src/main.js
+++ src/main.js
@@ -1,2 +1,19 @@
 "use strict";
 let firstName = "Dylan";
+let gender = true;
+console.log("firstname :" + firstName + " & gender=" + (gender === true ? "man" : "woman"));
+gender = 1;
+const names = [];
+names.push("mehdi");
+const car = {
+    type: "Toyota",
+    model: "Corolla",
+    year: 2009
+};
+car.type = "bemz";
+const car2 = { type: "Toyota" };
+car2.mileage = 2000;
+const nameAgeMap = {};
+nameAgeMap.Jack = 25;
+nameAgeMap.Mark = 24;
+nameAgeMap.mehdi = 22;
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,22 @@
 let firstName: string = "Dylan";
+let gender:any=true;
+console.log("firstname :"+firstName+" & gender="+(gender===true?"man":"woman"));
+gender=1;
+
+const names: string[]=[];
+names.push("mehdi");
+
+const car: { type: string, model: string, year: number } = {
+    type: "Toyota",
+    model: "Corolla",
+    year: 2009
+  };
+  car.type="bemz";
+
+  const car2: { type: string, mileage?: number } = {type: "Toyota"};
+car2.mileage = 2000;
+
+const nameAgeMap: { [index: string]: number } = {};
+nameAgeMap.Jack = 25;
+nameAgeMap.Mark = 24;
+nameAgeMap.mehdi=22;
